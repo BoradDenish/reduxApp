9041a90d96f8daa873caab714af060617a619769
Denish Borad <denishgithub4253@gmail.com>
2025-02-28 23:28:31 +0530

    some changes in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar'
+import Shop from './components/Shop'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Shop />
     </>
   )
 }
---
+++ src/components/Shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Shop() {
+  return (
+    <div>
+      <button className='btn btn-primary'> + </button>
+      Add this item to cart
+      <button className='btn btn-primary'> - </button>
+    </div>
+  )
+}
