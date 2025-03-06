3af6b703458058a14e8321fbecbe2910301ce31d
Borad Denish <denishgithub4253@gmail.com>
2025-03-06 21:52:42 +0530

    Update Shop.jsx

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -10,7 +10,7 @@ export default function Shop() {
     <div className='container'>
       <h3>Deposit/Withdraw Money</h3>
       {/* <button className='btn btn-primary mx-2' onClick={() => {dispatch(actionCreators.withdrawMoney(100))}}> - </button>
-      Update Balance
+      Update Balance {amount}
       <button className='btn btn-primary mx-2' onClick={() => {dispatch(actionCreators.depositMoney(100))}}> + </button> */}
 
       <button className='btn btn-primary mx-2' onClick={() => {withdrawMoney(100)}}> - </button>
