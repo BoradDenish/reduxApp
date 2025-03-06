7854f34111f25375f4a49e54755ece4333286966
Borad Denish <denishgithub4253@gmail.com>
2025-03-06 21:54:37 +0530

    Update Shop.jsx

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { actionCreators } from '../state/index'
-
+import { useSelector } from 'react-redux'
+    
 export default function Shop() {
+  const amount = useSelector(state => state.amount);
   const dispatch = useDispatch();
   const {depositMoney, withdrawMoney} = bindActionCreators(actionCreators, dispatch);
   return (
