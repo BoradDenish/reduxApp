4e1106fd8b9d46326559b2848cdd8853b7baf477
Denish Borad <denishgithub4253@gmail.com>
2025-03-07 08:58:20 +0530

    some issue fix

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -9,14 +9,14 @@ export default function Shop() {
   const dispatch = useDispatch();
   const {depositMoney, withdrawMoney} = bindActionCreators(actionCreators, dispatch);
   return (
-    <div className='container'>
+    <div className='container py-3'>
       <h3>Deposit/Withdraw Money</h3>
       {/* <button className='btn btn-primary mx-2' onClick={() => {dispatch(actionCreators.withdrawMoney(100))}}> - </button>
-      Update Balance {amount}
+      Update Balance is: {amount}
       <button className='btn btn-primary mx-2' onClick={() => {dispatch(actionCreators.depositMoney(100))}}> + </button> */}
 
       <button className='btn btn-primary mx-2' onClick={() => {withdrawMoney(100)}}> - </button>
-      Update Balance
+      Update Balance is: <strong>{amount}</strong>
       <button className='btn btn-primary mx-2' onClick={() => {depositMoney(100)}}> + </button>
     </div>
   )
