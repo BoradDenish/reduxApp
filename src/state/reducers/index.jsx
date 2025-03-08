60ff8b3207e11d42be6296541ca05703b4615cf6
Denish Borad <denishgithub4253@gmail.com>
2025-03-08 21:58:05 +0530

    add comments

--- src/state/reducers/index.jsx
+++ src/state/reducers/index.jsx
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import amountReducer from "./amountReducer";
 
+// call here redux function
 const reducers = combineReducers({
     amount: amountReducer
 })
--- src/state/store.jsx
+++ src/state/store.jsx
@@ -2,4 +2,5 @@ import { applyMiddleware, legacy_createStore as createStore } from "redux";
 import reducers from "./reducers";
 import { thunk } from "redux-thunk";
 
+// Create Redux Store
 export const store = createStore(reducers, {}, applyMiddleware(thunk))
