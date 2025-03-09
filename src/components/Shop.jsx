b321958e3df06576f684a48ec2a74122bd255de8
Denish Borad <denishgithub4253@gmail.com>
2025-03-09 22:10:02 +0530

    learn redux

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,21 +3,33 @@ import { useDispatch } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { actionCreators } from '../state/index'
 import { useSelector } from 'react-redux'
+
+import { increment, decrement, incrementByAmount } from '../state/counterSlice';
     
 export default function Shop() {
   const amount = useSelector(state => state.amount);
   const dispatch = useDispatch();
   const {depositMoney, withdrawMoney} = bindActionCreators(actionCreators, dispatch);
-  return (
-    <div className='container py-3'>
-      <h3>Deposit/Withdraw Money</h3>
-      {/* <button className='btn btn-primary mx-2' onClick={() => {dispatch(actionCreators.withdrawMoney(100))}}> - </button>
-      Update Balance is: {amount}
-      <button className='btn btn-primary mx-2' onClick={() => {dispatch(actionCreators.depositMoney(100))}}> + </button> */}
 
-      <button className='btn btn-primary mx-2' onClick={() => {withdrawMoney(100)}}> - </button>
-      Update Balance is: <strong>{amount}</strong>
-      <button className='btn btn-primary mx-2' onClick={() => {depositMoney(100)}}> + </button>
-    </div>
+  const count = useSelector((state) => state.counter.value);
+  return (
+    <>
+      <div className='container py-3'>
+        <h3>Deposit/Withdraw Money</h3>
+        {/* <button className='btn btn-primary mx-2' onClick={() => {dispatch(actionCreators.withdrawMoney(100))}}> - </button>
+        Update Balance is: {amount}
+        <button className='btn btn-primary mx-2' onClick={() => {dispatch(actionCreators.depositMoney(100))}}> + </button> */}
+
+        <button className='btn btn-primary mx-2' onClick={() => {withdrawMoney(100)}}> - </button>
+        Update Balance is: <strong>{amount}</strong>
+        <button className='btn btn-primary mx-2' onClick={() => {depositMoney(100)}}> + </button>
+      </div>
+      <div className='container py-3'>
+        <h1>Counter: {count}</h1>
+        <button className='btn btn-primary mx-2' onClick={() => dispatch(increment())}>+</button>
+        <button className='btn btn-primary mx-2' onClick={() => dispatch(decrement())}>-</button>
+        <button className='btn btn-primary mx-2' onClick={() => dispatch(incrementByAmount(5))}>+ by 5</button>
+      </div>
+    </>
   )
 }
--- src/state/store.jsx
+++ src/state/store.jsx
@@ -1,6 +1,18 @@
-import { applyMiddleware, legacy_createStore as createStore } from "redux";
-import reducers from "./reducers";
-import { thunk } from "redux-thunk";
+// import { applyMiddleware, legacy_createStore as createStore } from "redux";
+// import reducers from "./reducers";
+// import { thunk } from "redux-thunk";
 
 // Create Redux Store
-export const store = createStore(reducers, {}, applyMiddleware(thunk))
+// export const store = createStore(reducers, {}, applyMiddleware(thunk))
+
+
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from './counterSlice';
+
+export const store = configureStore({
+    reducer: {
+      counter: counterReducer
+    }
+  });
+
+
