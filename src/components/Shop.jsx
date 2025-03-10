06c9bd0dfa54672f3d57c138c86e46ff66c0da81
Denish Borad <denishgithub4253@gmail.com>
2025-03-10 23:28:32 +0530

    add new redux code for learn

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { actionCreators } from '../state/index'
 import { useSelector } from 'react-redux'
 
 import { increment, decrement, incrementByAmount } from '../state/counterSlice';
+import { addTodo, toggleTodo, deleteTodo } from '../state/todoSlice';
     
 export default function Shop() {
+  const [text, setText] = useState('');
+  const todos = useSelector((state) => state.todos);
+
   const amount = useSelector(state => state.amount);
   const dispatch = useDispatch();
   const {depositMoney, withdrawMoney} = bindActionCreators(actionCreators, dispatch);
 
   const count = useSelector((state) => state.counter.value);
+
+  const handleAddTodo = () => {
+    if (text.trim()) {
+      dispatch(addTodo(text));
+      setText('');
+    }
+  };
+
   return (
     <>
       <div className='container py-3'>
@@ -30,6 +42,28 @@ export default function Shop() {
         <button className='btn btn-primary mx-2' onClick={() => dispatch(decrement())}>-</button>
         <button className='btn btn-primary mx-2' onClick={() => dispatch(incrementByAmount(5))}>+ by 5</button>
       </div>
+      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      <h2>📝 Redux Todo App</h2>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Add a new todo..."
+      />
+      <button onClick={handleAddTodo}>Add</button>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+            {todo.text}
+            <button onClick={() => dispatch(toggleTodo(todo.id))}>
+              {todo.completed ? 'Undo' : 'Complete'}
+            </button>
+            <button onClick={() => dispatch(deleteTodo(todo.id))}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
     </>
   )
 }
--- src/state/store.jsx
+++ src/state/store.jsx
@@ -8,10 +8,13 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from './counterSlice';
+import todoReducer from './todoSlice';
 
 export const store = configureStore({
     reducer: {
-      counter: counterReducer
+      counter: counterReducer,
+      todos: todoReducer
+
     }
   });
 
